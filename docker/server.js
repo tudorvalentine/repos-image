58c979789f624b0e0f98cdab0593ee2ae6665cd0
tudorvalentine <tudor1903@gmail.com>
2021-06-17 14:20:33 +0300

    Revert "Update to version two"
    This reverts commit 412841e3fe8ec427896ba94315849ebf65ca0e61.

--- docker/server.js
+++ docker/server.js
@@ -1,15 +1,9 @@
 var http = require('http');
-var startTime;
-var host;
+
 var handleRequest = function(request, response) {
-  response.setHeader('Content-Type', 'text/plain');
+  console.log('Am primit un request URL ' + request.url);
   response.writeHead(200);
-  response.write("Hello World , este versiunea II! | Ruleaza pe :");
-  response.write(host);
-  response.end(" v=2\n");
-  console.log("Running On:" , host);
-}
+  response.end('Hello World! It first Image.');
+};
 var www = http.createServer(handleRequest);
-www.listen(8080,function () {
-    host = process.env.HOSTNAME;
-});
+www.listen(8080);
